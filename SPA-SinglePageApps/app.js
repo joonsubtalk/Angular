a5a2c8fe0cd2ea408de95908b832a9c077ac7eeb
joonsub <joonsubtalk@gmail.com>
2016-02-04 13:14:16 -0800

    understanding instances of $scope per controller.

--- SPA-SinglePageApps/app.js
+++ SPA-SinglePageApps/app.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', function($scope){
-                          
+    $scope.greet = "hello!";
+}]);
+
+// This $scope is a new instance of the $scope object. Has nothing to do with mainController.
+myApp.controller('secondController', ['$scope', function($scope){
+    $scope.greet = "aloha!";     
 }]);
