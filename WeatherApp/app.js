2fe4e31ada36812fb2fbfc361945facf0145b3e7
joonsub <joonsubtalk@gmail.com>
2016-02-09 20:21:46 -0800

    Wireup the two model and views

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,6 +1,23 @@
 // Module
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
+// Routing
+// inject routeProvider to function
+weatherApp.config(function($routeProvider){
+                  
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'homeController'
+    })
+    .when('/forecast', {
+        templateUrl: 'partials/forecast.html',
+        controller: 'forecastController'
+    })          
+                  
+});
+
 //Controllers
 weatherApp.controller('homeController', ['$scope', function($scope){
     
