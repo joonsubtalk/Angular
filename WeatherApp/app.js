72bdf5389db2e41c78019c9b16bd8a6bed07faec
joonsub <joonsubtalk@gmail.com>
2016-02-09 20:18:11 -0800

    Add Controllers for the two pages.

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,2 +1,11 @@
 // Module
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+//Controllers
+weatherApp.controller('homeController', ['$scope', function($scope){
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function($scope){
+    
+}]);
