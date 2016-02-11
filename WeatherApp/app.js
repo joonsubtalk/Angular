b0ef12513fcdd08c6eb1f1f35051cfdb5f0fba0e
Joon-Sub Chung <joonsubtalk@gmail.com>
2016-02-11 08:45:40 -0800

    woohoo data! from an API

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,4 +1,5 @@
-// Module
+// Module // fyi ngResource allows you to get data from the Internet! :D
+// ngResource wraps up http! Easier data grabb'n
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 // Routing
@@ -24,18 +25,30 @@ weatherApp.service('cityService', function(){
 });
 
 //Controllers
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('homeController', ['$scope', '$resource', 'cityService', function($scope, $resource,cityService){
     
     $scope.city = cityService.city;
+    
+    $scope.weatherAPI = 
+        $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {
+        callback: "JSON_CALLBACK"}, { get : {method:"JSONP"}});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({
+        //APPID: "",
+        q: $scope.city,
+        cnt: 2
+    }); // gets the data and can pass obj for params
     
     // watch
     $scope.$watch('city', function(){
        cityService.city = $scope.city; 
     });
+    
+    console.log($scope.weatherResult)
 }]);
 
 weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
     $scope.city = cityService.city;    
 }]);
 
-//http://api.openweathermap.org/data/2.5/forecast/daily?APPID=
+//http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=8a54b0712916ede57e21659b51b80f59
